43d062c9f51592442c2c80ca43cf16bf411fe0e5
ljjhhhi <96717686+ljjhhhi@users.noreply.github.com>
2022-01-12 10:58:57 +0900

    Create 2292.py

---
+++ level8/2292.py
@@ -0,0 +1,13 @@
+i=int(input())
+if i==1:
+    print(1)
+elif i>=2 and i<=7:
+    print(2)
+elif i>=8 and i<=19:
+    print(3)
+elif i>=20 and i<=37:
+    print(4)
+elif i>=38 and i>=61:
+    print(5)
+elif i>=62 and i<=70:
+    print(6)
